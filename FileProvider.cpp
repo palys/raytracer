06125cbfde7d917b181af16f55de2d23f75cdf26
Tomasz Palys <tomek201992@gmail.com>
2021-01-24 14:21:41 +0100

    Better file name

--- FileProvider.cpp
+++ FileProvider.cpp
@@ -1,32 +1,27 @@
 #include "FileProvider.hpp"
 
 #include <fstream>
-#include <chrono>
-#include <ctime>
 #include <sstream>
-
+#include <ctime>
 #include <iostream>
 
 FileProvider::FileProvider(): dir("out") {
 
 }
 
-std::string sanitize(std::string text) {
-    std::string s;
-    for (auto & c : text) {
-        if (c != ' ' && c != '\n' && c!= '\r' && c != ':') {
-            s += c;
-        }
-    }
-    return s;
-}
-
 std::ofstream FileProvider::new_file() {
-    auto now = std::chrono::system_clock::now();
-    auto t_c = std::chrono::system_clock::to_time_t(now);
+    time_t now = std::time(nullptr);
     std::stringstream name;
-    name << dir << "/" << std::ctime(&t_c) << ".tga";
-    auto file_name = sanitize(name.str());
+    auto time = std::localtime(&now);
+    name << dir << "/" 
+        << time->tm_year + 1900 << "-" 
+        << time->tm_mon + 1 << "-"
+        << time->tm_mday << "-" 
+        << time->tm_hour << "-"
+        << time->tm_min << "-"
+        << time->tm_sec
+        << ".tga";
+    auto file_name = name.str();
 
     std::ofstream file;
     file.open(file_name);
